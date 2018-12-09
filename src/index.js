24bf792c41b7485b91a42aca93226b724bc9e888
Ahsan-J <ahsan.ahmed99@ymail.com>
2018-12-09 20:10:32 +0500

    App Index : Navigator State change added for active route drawer

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, StatusBar } from 'react-native';
 import Navigator from './navigator/routes.js'
-import {getDeviceHeight,getDeviceWidth} from './helpers/sizeHelpers.js'
+import { getDeviceHeight, getDeviceWidth } from './helpers/sizeHelpers.js'
 import SplashScreen from 'react-native-splash-screen'
 import Drawer from './components/drawer/drawer.js'
+import { getActiveRouteName, setTopLevelNavigator } from './helpers/navigationHelper.js'
+import { connect } from 'react-redux'
+import { setActiveRoute } from './redux/actions/general.js';
+import { closeDrawer } from './helpers/drawerHelper.js';
 
 // Global Component to handle global files
-export default class App extends Component {
+class App extends Component {
 
-  componentDidMount () {
+  componentDidMount() {
     SplashScreen.hide();
   }
 
   render() {
     return (
       <Drawer style={styles.container}>
-        <StatusBar hidden/>
-        <Navigator />
+        <StatusBar hidden />
+        <Navigator
+          ref={navigator => setTopLevelNavigator(navigator)}
+          onNavigationStateChange={(prevState, currentState) => {
+            const currentScreen = getActiveRouteName(currentState);
+            const prevScreen = getActiveRouteName(prevState);
+
+            if (prevScreen !== currentScreen) {
+              this.props.setActiveRoute(currentScreen);
+              closeDrawer();
+            }
+          }}
+        />
       </Drawer>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setActiveRoute: (route) => dispatch(setActiveRoute(route))
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
-    height : getDeviceHeight(),
-    width : getDeviceWidth(),
+    height: getDeviceHeight(),
+    width: getDeviceWidth(),
   },
 });
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
