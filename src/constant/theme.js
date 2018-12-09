cdd5f1ae2978be8f7c73675d7050138a6732acc2
Ahsan-J <ahsan.ahmed99@ymail.com>
2018-12-09 20:11:45 +0500

    Theme: Font colors defined

--- src/constant/theme.js
+++ src/constant/theme.js
@@ -3,5 +3,6 @@ export default {
   successColor : "#3FC856",
   dangerColor : "#DB434F",
   blackColor : "#2B2924", // Rasin Black
-
+  fontPrimaryColor : "#ffffff",
+  fontSecondaryColor : "#000000",
 }
