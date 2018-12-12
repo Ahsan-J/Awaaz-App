63a59828175fbbc3be5d1a52772d55c37b3d484a
Ahsan-J <ahsan.ahmed99@ymail.com>
2018-12-13 01:38:54 +0500

    Theme : new theme combo added

--- src/constant/theme.js
+++ src/constant/theme.js
@@ -1,8 +1,22 @@
 export default {
   primaryColor : "#B32E2E", // Red-Brown
-  successColor : "#3FC856",
-  dangerColor : "#DB434F",
-  blackColor : "#2B2924", // Rasin Black
+
+  // successColor:  "#5cb85c", // theme : 1
+  // dangerColor:   "#d9534f", // theme : 1
+  // warnColor:  "#f0ad4e", // theme 1
+  // infoColor:          "#5bc0de", // theme : 1
+  
+  successColor:  "#00bc8c",  // theme : 2
+  dangerColor:   "#E74C3C", // theme : 2
+  warnColor:  "#F39C12", // theme 2
+  infoColor:          "#3498DB", // theme : 2
+
+  // successColor:  "#62c462", // theme : 3
+  // dangerColor:   "#EE5F5B", // theme : 3
+  // warnColor:  "#f89406", // theme 3
+  // infoColor:          "#5bc0de", // theme : 3
+
+  secondaryColor : "#2B2924", // Rasin Black
   fontPrimaryColor : "#ffffff",
   fontSecondaryColor : "#000000",
 }
