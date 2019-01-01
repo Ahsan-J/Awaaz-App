b3dfe07244756f0f27955d245664d2b0d89ed6f0
Ahsan-J <ahsan.ahmed99@ymail.com>
2019-01-02 00:45:53 +0500

    Report_03 : initial layout

---
+++ src/screens/Report_03/Report_03.js
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { 
+  View,
+  TouchableOpacity
+} from 'react-native';
+import f from './function.js'
+import styles from './style.js'
+import Header from '../../components/header/mainHeader';
+import { 
+  Container, 
+  Form, 
+  Item,
+  Label,
+  Input,
+  Button,
+  Text,
+  Content,
+  Radio
+} from 'native-base'
+import { openDrawer } from '../../helpers/drawerHelper.js';
+import State from '../../components/reportState/state.js';
+import NumberSpinner from '../../components/numberSpinner/numberSpinner.js';
+
+class Report_03 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      victims : 0,
+      selfVictim : true,
+    }
+    this.getVictimCount = f.getVictimCount.bind(this);
+    this.onValueChange = f.onValueChange.bind(this);
+    this.renderVictimFields = f.renderVictimFields.bind(this)
+    this.componentDidMount = f.componentDidMount.bind(this);
+  }
+  render() {
+    return (
+      <Container>
+        <Header
+          leftCallBack={() => openDrawer()}
+          title="Report" />
+        <Content contentContainerStyle={styles.contentContainer} style={styles.container}>
+          <State state={3} />
+          <View style={styles.headingTextWrapper}>
+            <Text style={styles.headingText}>Victim Details</Text>
+          </View>
+          <Form style={styles.form}>
+            <View style={styles.victimsControls}>
+              <View style={styles.radioQuestionWrapper}>
+                <View style={{flex:0.6}}>
+                  <Text style={styles.radioText}>Are you the victim ?</Text>
+                </View>
+                <TouchableOpacity style={styles.radioOption} onPress={()=> this.setState({selfVictim:true})}>
+                  <Text style={styles.radioText}>Yes</Text><Radio onPress={()=> this.setState({selfVictim:true})} selected={this.state.selfVictim} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.radioOption} onPress={()=> this.setState({selfVictim:false})}>
+                  <Text style={styles.radioText}>No</Text><Radio onPress={()=> this.setState({selfVictim:false})} selected={!this.state.selfVictim} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.radioQuestionWrapper}>
+                <Text style={[styles.radioText]}>Number of Victim(s)</Text>
+                <NumberSpinner value={this.state.victims} step={1} onChange={this.getVictimCount}/>
+              </View>
+            </View>
+            {this.renderVictimFields()}
+          </Form>
+          <View style={styles.buttonGroup}>
+            <Button style={styles.skipButton}>
+              <Text uppercase={false} style={styles.skipText}>Skip</Text>
+            </Button>
+            <Button style={styles.nextButton} onPress={this.onNext}>
+              <Text uppercase={false} style={styles.nextText}>Next</Text>
+            </Button>
+          </View>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+export default connect(f.mapStateToProps.bind(this), f.mapDispatchToProps.bind(this))(Report_03)
---
+++ src/screens/Report_03/function.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import _ from 'lodash';
+import styles from './style';
+import {
+  View
+} from 'react-native';
+import {
+  Item,
+  Label,
+  Input,
+  Picker,
+  DatePicker,
+  Icon as NBIcon,
+  Text
+} from 'native-base';
+import ReportStore from './store';
+import { moderateScale } from '../../helpers/sizeHelpers';
+
+export default {
+
+  /**********************LifeCycle Components in Order **********************************/
+
+  componentDidMount: function () {
+
+  },
+
+  /***********************Additonal Functions to be used within component ***************/
+
+  getVictimCount: function (victims) {
+    this.setState({ victims });
+  },
+
+  onValueChange: function () {
+
+  },
+
+  renderVictimFields: function () {
+    return _.map(_.range(0, this.state.victims), (index) => {
+      return (
+        <View key={index} style={{minHeight:moderateScale(50),paddingTop : moderateScale(5),paddingBottom:moderateScale(5)}}>
+          <Text style={styles.radioText}>{index+1 + " )"}</Text>
+          <Item stackedLabel last >
+            <Label style={styles.label}>Name</Label>
+            <Input style={styles.input} />
+          </Item>
+          <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+            <Item picker stackedLabel last style={[styles.item,{flex:0.5}]}>
+              <Label style={styles.label}>Gender</Label>
+              <View style={styles.input}>
+                <Picker
+                  mode="dropdown"
+                  iosIcon={<NBIcon name="ios-arrow-down-outline" />}
+                  style={{ color: '#ffffff' }}
+                  placeholder="Gender"
+                  placeholderStyle={{ color: "#ffffff" }}
+                  placeholderIconColor="#ffffff"
+                  selectedValue={this.state.gender}
+                  onValueChange={this.onValueChange}
+                >
+                  <Picker.Item label="Male" value="key0" />
+                  <Picker.Item label="Female" value="key1" />
+                  <Picker.Item label="Others" value="key2" />
+                </Picker>
+              </View>
+            </Item>
+            <Item picker stackedLabel last style={[styles.item,{flex:0.5}]}>
+              <Label style={styles.label}>Date of birth</Label>
+              <View style={styles.input}>
+                <DatePicker
+                  defaultDate={new Date(2018, 4, 4)}
+                  locale={"en"}
+                  timeZoneOffsetInMinutes={undefined}
+                  modalTransparent={true}
+                  animationType={"fade"}
+                  androidMode={"default"}
+                  placeHolderText="DD / MM / YYYY"
+                  textStyle={{ color: "#ffffff" }}
+                  placeHolderTextStyle={{ color: "#d3d3d3" }}
+                  onDateChange={this.setDate}
+                />
+              </View>
+            </Item>
+          </View>
+        </View>
+      )
+    })
+  },
+
+  /**********************Connecting and defining the Redux ******************************/
+  mapStateToProps: function (state) {
+    return {
+
+    }
+  },
+
+  mapDispatchToProps: function (dispatch) {
+    return {
+
+    }
+  },
+}
+
+// Local functions limitng to only this component
