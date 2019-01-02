925b856f629bc553b9f94a7aad0ddc3aa576f406
Ahsan-J <ahsan.ahmed99@ymail.com>
2019-01-02 23:13:28 +0500

    RecordNow : initial Layout created

--- src/screens/RecordNow/RecordNow.js
+++ src/screens/RecordNow/RecordNow.js
@@ -8,12 +8,19 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { View } from 'react-native';
+import { 
+  View,
+  TouchableOpacity,
+  Text
+ } from 'react-native';
 import f from './function.js'
 import styles from './style.js'
 import Header from '../../components/header/mainHeader';
-import {Container} from 'native-base'
+import {
+  Container,
+} from 'native-base'
 import { openDrawer } from '../../helpers/drawerHelper.js';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class RecordNow extends Component {
   constructor(props) {
@@ -28,9 +35,22 @@ class RecordNow extends Component {
       <Container>
         <Header
           leftCallBack={()=> this.props.navigation.goBack()} 
-          title="Record Now"/>
+          title="Record"/>
         <View style={styles.container}>
-         
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.buttonTouchables}>
+              <Icon name="microphone" style={styles.buttonIcon}/>
+              <Text style={styles.buttonText}>Start secret audio recording</Text>
+            </TouchableOpacity>
+            <TouchableOpacity  style={styles.buttonTouchables}>
+              <Icon name="video-camera" style={styles.buttonIcon}/>
+              <Text style={styles.buttonText}>Start secret video recording</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.textMainView}>
+            <Text style={styles.textHeading}>How it works?</Text>
+            <Text style={styles.textParagraph}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione excepturi fugiat officia enim voluptatum quas accusantium accusamus quibusdam necessitatibus, maxime nostrum, explicabo magni consectetur omnis aliquid. Qui ipsam fugiat numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis omnis, reprehenderit, blanditiis maxime ullam culpa et laborum dicta earum commodi minus voluptatum exercitationem? Ipsum nisi accusamus sapiente iure corrupti.</Text>
+          </View>
         </View>
       </Container>
     );
--- src/screens/RecordNow/style.js
+++ src/screens/RecordNow/style.js
@@ -1,21 +1,47 @@
 import {StyleSheet} from 'react-native'
 import theme from '../../constant/theme';
+import { moderateScale, getDeviceWidth, verticalScale } from '../../helpers/sizeHelpers';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: theme.secondaryColor,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  buttonContainer:{
+    flexDirection : 'row',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#ffffff',
-    marginBottom: 5,
+  buttonTouchables : {
+    height : getDeviceWidth()/2,
+    width : getDeviceWidth()/2,
+    borderColor : theme.primaryColor,
+    borderWidth : moderateScale(1),
+    justifyContent : 'center',
+    alignItems : 'center',
   },
+  buttonIcon :{
+    color : theme.fontPrimaryColor,
+    fontSize : moderateScale(45),
+  },
+  buttonText : {
+    color : theme.fontPrimaryColor,
+    textAlign : 'center',
+    fontSize : moderateScale(18),
+    marginTop : verticalScale(10),
+  },
+  textMainView:{
+    flex:1,
+    // justifyContent:'space-around',
+  },
+  textHeading : {
+    marginTop : verticalScale(25),
+    marginBottom : verticalScale(25),
+    fontSize : moderateScale(20),
+    color : theme.fontPrimaryColor,
+  },
+  textParagraph : {
+    fontSize : moderateScale(15),
+    color : theme.fontPrimaryColor,
+  }
 });
